c14169ad4afb86c55b24ff72b65021edf61fa4de
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-07-03 09:59:46 +0200

    Kreiran interfejs Contact i osnove CRUD operacije

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+export interface Contact {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private contacts: Contact[] = [];
+  private nextId = 1;
 
   constructor() { }
+
+  getContacts(): Contact[] {
+    return this.contacts;
+  }
+
+  getContact(id: number): Contact | undefined {
+    return this.contacts.find(contact => contact.id === id);
+  }
+  addContact(contact: Contact): void {
+    contact.id = this.nextId++;
+    this.contacts.push(contact);
+  }
+  updateContact(contact: Contact): void {
+    const index = this.contacts.findIndex(c => c.id === contact.id);
+    if (index !== -1) {
+      this.contacts[index] = contact;
+    }
+  }
+  deleteContact(id: number): void {
+    this.contacts = this.contacts.filter(contact => contact.id !== id);
+  }
 }
